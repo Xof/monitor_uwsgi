971cc1f861f54092f49074fc7492e462789d6a3a
Christophe Pettus <cpettus@pgexperts.com>
2026-07-11 16:36:32 -0700

    Add apps collector (per-app metrics for multi-app workers)

--- datadog_checks/uwsgi_stats/metrics/__init__.py
+++ datadog_checks/uwsgi_stats/metrics/__init__.py
@@ -1,4 +1,5 @@
 from .aggregate import collect as collect_aggregate
+from .apps import collect as collect_apps
 from .sockets import collect as collect_sockets
 from .workers import collect as collect_workers
 
@@ -6,4 +7,5 @@ COLLECTORS = [
     collect_aggregate,
     collect_sockets,
     collect_workers,
+    collect_apps,
 ]
